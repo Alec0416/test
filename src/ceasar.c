a2284c39dcdc9db04988eb02056e33229c5b6fbf
alec0416 <alec.hsu@stonybrook.edu>
2023-10-11 23:48:21 -0400

    withouttests

--- src/ceasar.c
+++ src/ceasar.c
@@ -2,5 +2,91 @@
 #include "../include/global.h" 
 
 
-int encrypt(const char *plaintext, char *ciphertext, int key);
-int decrypt(const char *ciphertext, char *plaintext, int key);
+int encrypt(const char *plaintext, char *ciphertext, int key){
+    int encoded = 0;
+    char *cipherOriginal = ciphertext;
+    if(plaintext==NULL || ciphertext==NULL){  //checks to see if both characters are null, if not then continues
+        return -2;
+    }
+    while(*plaintext!= '\0' && *ciphertext!= '\0'){ //loops until either address contents are null character
+        if(plaintext==NULL || ciphertext==NULL){  //checks to see if both characters are null, if not then continues
+            return -2;
+        }
+        if(isupper(*plaintext)){ //encrypts them and increments depending on type of character
+            *ciphertext =  ((*plaintext+key-'A')%26)+'A';
+            encoded++;
+            ciphertext++;
+        }
+        else if(islower(*plaintext)){
+            *ciphertext = ((*plaintext+key-'a')%26)+'a';
+            encoded++;
+            ciphertext++;
+        }
+        else if (*plaintext>='0' && *plaintext<='9'){
+            *ciphertext = ((*plaintext+key-'0')%10)+'0';
+            encoded++;
+            ciphertext++;
+        }
+        else { // if it is not alphanumeric, do not encrypt
+            *ciphertext = *plaintext;
+            ciphertext++;
+        }
+        plaintext++;
+    }
+    if(encoded == 0){ //returns 0 if no characters are encoded
+        strgCopy("undefined__EOM__",cipherOriginal);
+        return 0;
+    }
+    if(strgLen(ciphertext)<7){ //returns -1 and attempts to copy the rest of the EOM marker if there is no space for the EOM marker
+        strgCopy("__EOM__",ciphertext);
+        return -1;
+    }
+    strgCopy("__EOM__",ciphertext); //copies the EOM marker
+    return encoded; //returns encoded characters
+}
+int decrypt(const char *ciphertext, char *plaintext, int key){
+    const char *eom = ciphertext;
+    int decoded = 0;
+    if(plaintext==NULL || ciphertext==NULL){  //checks to see if both characters are null, if not then continues
+        return -2;
+    }
+    if(*eom == '\0'){
+        return -1; //returns -1 if ciphertext is empty 
+    } 
+    while(*eom!='\0'){ //iterates through ciphertext to find EOM marker
+        if(*eom == '\0'|| *(eom+1) == '\0' || *(eom+2) == '\0'|| *(eom+3) == '\0'|| *(eom+4) == '\0'|| *(eom+5) == '\0'|| *(eom+6) == '\0'){
+            return -1; //returns -1 if null character is reached before EOM marker
+        }
+        else if(*eom == '_'&& *(eom+1) == '_' && *(eom+2) == 'E'&& *(eom+3) == 'O'&& *(eom+4) == 'M'&& *(eom+5) == '_'&& *(eom+6) == '_'){
+            break; //saves EOM address in *eom if found
+        }
+        eom++;
+    }
+    while(ciphertext!=eom){ //iterates until address of ciphertext is equal to eom address
+        if(*plaintext == '\0'){ //breaks if plaintext cannot fit message
+            break;
+        }
+        if(isupper(*ciphertext)){ //decrypts them and increments depending on type of character
+            *plaintext = (*ciphertext-key-'A'+26)%26+'A'; 
+            decoded++;
+            plaintext++;
+        }
+        else if(islower(*ciphertext)){
+            *plaintext = (*ciphertext-key-'a'+26)%26+'a'; 
+            decoded++;
+            plaintext++;
+        }
+        else if (*ciphertext>='0' && *ciphertext<='9'){
+            *plaintext = (*ciphertext-key-'0'+10)%10+'0'; 
+            decoded++;
+            plaintext++;
+        }
+        else { // if it is not alphanumeric, do not decrypt
+            *plaintext = *ciphertext;
+            plaintext++;
+        }
+        ciphertext++;
+    }
+    *plaintext = '\0';
+    return decoded;
+}
--- src/strPtr.c
+++ src/strPtr.c
@@ -2,21 +2,78 @@
 
 int strgLen(char *s){
     // TO BE IMPLEMENTED
-    abort();
+    int len = 0;
+    while(*s!= '\0'){ //checks if the address contents is null character
+        len++; // adds one to length 
+        s++; // proceeds to next character in string
+    }
+    return len;
 }
 void strgCopy(char *s,char *d ){
     // TO BE IMPLEMENTED
-    abort();
+    if(d!=NULL || s!=NULL){ //checks to see if both characters are null, if not then continues
+    while(*s!= '\0' && *d!= '\0'){ //checks if either address contents are null character
+        *d = *s; //puts s contents in d
+        s++; //increments s
+        d++; //increments d
+    }
+    *d = '\0'; //ends d with the null character once s is finished copying
+    }
 }
 void strgChangeCase(char *s){
     // TO BE IMPLEMENTED
-    abort();
+    while(*s!='\0'){ //iterates through string until null character
+        if(islower(*s)){ //sets lower to upper
+            *s = toupper(*s);
+        }
+        else if(isupper(*s)){ //sets upper ot lower 
+            *s = tolower(*s);
+        }
+        s++; 
+    }
+
 }
 int strgDiff(char *s1,char *s2 ){
     // TO BE IMPLEMENTED
-    abort();
+    if(s1==NULL || s2 == NULL || *s1 == '\0' || *s2 == '\0'){ //returns -2 if either character is null or empty string
+        return -2;
+    }
+    int index = 0;
+    while(*s1!='\0' && *s2!='\0'){// iterates until end of either string
+        if(*s1!=*s2){ //returns index if character is different
+            return index;
+        }
+        index++; 
+        s1++;
+        s2++;
+    }
+    return -1; //if no character is different, return -1
+
 }
 void strgInterleave(char *s1, char *s2, char *d ){
     // TO BE IMPLEMENTED
-    abort();
+    if(s1==NULL || s2 == NULL || d == NULL){ // returns if any address is NULL
+        return;
+    }
+    while(*s1!='\0' && *s2!='\0' && *d!='\0'){ //iterates until end of either string or until d has no space
+        *d = *s1; //puts s1 contents in d
+        d++;
+        if(*d=='\0'){
+            break; //breaks if d has no space after incrementing d once
+        }
+        *d = *s2; //puts s2 contents in d after increment
+        d++; 
+        s1++; //increment all three 
+        s2++;
+    }
+    if(*s1=='\0' && *s2!='\0'){ //if end of one string is reached but not the other, copy rest of other string to d
+        strgCopy(s2,d);
+    }
+    else if(*s2=='\0' && *s1!='\0'){
+        strgCopy(s1,d);
+    }
+    else{ // only if s1 and s2 are the same length or are both empty, the contents of d are set to null character
+        *d = '\0';
+    }
+
 }
